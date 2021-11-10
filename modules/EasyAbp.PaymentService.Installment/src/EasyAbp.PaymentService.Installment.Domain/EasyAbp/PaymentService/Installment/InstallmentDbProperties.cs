c97238015d78d3f616dc1927be4a2225fb772ef4
slarkerino <wanjackcool@qq.com>
2021-11-10 14:46:04 +0800

    Modified Connection String Name For Installment

--- modules/EasyAbp.PaymentService.Installment/src/EasyAbp.PaymentService.Installment.Domain/EasyAbp/PaymentService/Installment/InstallmentDbProperties.cs
+++ modules/EasyAbp.PaymentService.Installment/src/EasyAbp.PaymentService.Installment.Domain/EasyAbp/PaymentService/Installment/InstallmentDbProperties.cs
@@ -2,10 +2,10 @@
 {
     public static class InstallmentDbProperties
     {
-        public static string DbTablePrefix { get; set; } = "Installment";
+        public static string DbTablePrefix { get; set; } = "EasyAbpPaymentServiceInstallment";
 
         public static string DbSchema { get; set; } = null;
 
-        public const string ConnectionStringName = "Installment";
+        public const string ConnectionStringName = "EasyAbpPaymentServiceInstallment";
     }
 }
